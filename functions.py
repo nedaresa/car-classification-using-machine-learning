486691e8c9000ecd0492e776fa8a88c488471774
Juan Julian <jjherranzs@gmail.com>
2019-09-04 15:21:03 -0700

    Reduced the number of functions from 2 to 1 to prevent creating a class

--- functions.py
+++ functions.py
@@ -3,19 +3,15 @@
 import pandas as pd
 import numpy as np
 
-def split_market_categories(df):
-    """Function that splits each car's market category into a single list
-    containing all market categories."""
-    categories = []
-    for category in df['Market Category']:
-        categories += category.split(',')
-    return categories
-
 def market_columns(df):
     """Function that maps multiple entries in a column into individual columns
     and assings a score of 1 or 0 if the entry is question is within a given row.
     """
-    unique = set(split_market_category(df))
+    categories = []
+    for category in list(df['Market Category'].unique()):
+        categories += category.split(',')
+
+    unique = set(categories)
     for col in unique:
         df[col] = df['Market Category'].apply(lambda x: 1 if col in x.split(',') else 0)
     df.drop('Market Category', axis=1, inplace=True)
